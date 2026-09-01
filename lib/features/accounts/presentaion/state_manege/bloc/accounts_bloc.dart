626e9ffb0e6a893168e27648630e2edf1ef32cc4
Ahmad Nagy <ahma4dna@gmail.com>
2026-09-01 10:33:38 +0200

    feat(account):add insert wallet to bloc

--- lib/features/accounts/presentaion/state_manege/bloc/accounts_bloc.dart
+++ lib/features/accounts/presentaion/state_manege/bloc/accounts_bloc.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
+
+import 'package:fintrack/features/accounts/domain/use_case/insert_wallet_use_case.dart';
 import 'package:fintrack/features/accounts/presentaion/state_manege/bloc/accounts_event.dart';
 import 'package:fintrack/features/accounts/presentaion/state_manege/bloc/accounts_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
 
+@lazySingleton
 class AccountsBloc extends Bloc<AccountsEvent, AccountsState> {
-  AccountsBloc() : super(AccountsState()) {
-    on<AccountsEvent>((event, emit) {});
+  final InsertWalletUseCase insertWalletUseCase;
+  AccountsBloc({required this.insertWalletUseCase}) : super(AccountsState()) {
+    on<InertWalletEvent>(_insertWallet);
+  }
+
+  Future<void> _insertWallet(InertWalletEvent event, Emitter<AccountsState> emit) async {
+    await insertWalletUseCase.insertWallet(event.walletModel);
   }
 }
